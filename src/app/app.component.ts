37c676d3cea6e36bb91bb6477875ca559840b995
Mitch Kett <mitchkett@gmail.com>
2018-09-15 22:58:25 -0400

    Initial component for map display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'Unknown Territory';
+  lat: number = 42.9429479;
+  lng: number = -85.6801226;
 }
