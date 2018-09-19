b056faa89901a89e871e5aace9b5948e647a95ef
Mitch Kett <mitchkett@gmail.com>
2018-09-19 00:04:26 -0400

    Service to return map layers for territory browsing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { AgmCoreModule } from '@agm/core';
+import { AgmCoreModule, DataLayerManager, GoogleMapsAPIWrapper } from '@agm/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatInputModule, MatFormFieldModule, MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule } from '@angular/material';
+import {
+	MatInputModule, MatFormFieldModule, MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule, MatSelectModule, MatMenuModule
+} from '@angular/material';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
@@ -14,6 +16,7 @@ import { RepresentativesService } from './services/representatives.service';
 import { OfficeComponent } from './components/office/office.component';
 import { OfficialComponent } from './components/official/official.component';
 import { ChannelLinkComponent } from './components/channel-link/channel-link.component';
+import { TerritoriesService } from './services/territories.service';
 
 @NgModule({
 	declarations: [
@@ -34,12 +37,14 @@ import { ChannelLinkComponent } from './components/channel-link/channel-link.com
 		MatToolbarModule,
 		MatIconModule,
 		MatCardModule,
+		MatSelectModule,
+		MatMenuModule,
 		FontAwesomeModule,
 		AgmCoreModule.forRoot({
 			apiKey: 'AIzaSyA_R63kyjU_cCRxVOKpqKNG3qfys10LsvY'
 		})
 	],
-	providers: [RepresentativesService],
+	providers: [DataLayerManager, GoogleMapsAPIWrapper, RepresentativesService, TerritoriesService],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/territories.service.ts
+++ src/app/services/territories.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class TerritoriesService {
 
-  constructor() { }
+	constructor(private http: HttpClient) { }
+
+	getStates(): Observable<Object> {
+		return this.http.get<Object>('./assets/us-states.geojson');
+	}
 }
