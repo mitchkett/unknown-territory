2c6ecade1c43d71d56a174c7853b46e19b6a0461
Mitch Kett <mitchkett@gmail.com>
2018-09-20 21:27:18 -0400

    Production settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-	production: true
+	production: true,
+	googleCivicApiKey: 'AIzaSyDpgKmm2Om9gf2NqyTIzHIobwXz0WyvORk',
+	googleMapsApiKey: 'AIzaSyA_R63kyjU_cCRxVOKpqKNG3qfys10LsvY'
 };
